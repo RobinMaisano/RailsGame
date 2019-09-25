106ed717603810d0f41e9d77e87fa6993b79fa5d
Robin Maisano <robin.maisano@gmail.com>
2019-09-25 15:54:49 +0200

    <Heroku> Update mailer host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '"stark-wave-18256".heroku.com' }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
